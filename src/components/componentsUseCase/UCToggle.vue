472d879f14c32c0e776c09acf4b2769b35d008da
Vik <victorgou.design@gmail.com>
2024-09-17 13:27:00 -0300

    UCToggle for the Component Documentation

---
+++ src/components/componentsUseCase/UCToggle.vue
@@ -0,0 +1,21 @@
+<template>
+    <h3>Toggle Component</h3>
+    <p>The Toggle Component is a customizable switch that allows users to toggle between two states.</p>
+    <br>
+    <Toggle v-model="isOnline" :icon="toggleIcon" />
+    <p>Current status: {{ isOnline ? 'Online' : 'Offline' }}</p>
+  </template>
+  
+  <script setup>
+  import { ref, computed } from 'vue';
+  import Toggle from '@/components/Toggle.vue';
+  import { Wifi, WifiOff } from 'lucide-vue-next';
+  
+  const isOnline = ref(false);
+  
+  // Compute the icon based on the current online status
+  const toggleIcon = computed(() => isOnline.value ? Wifi : WifiOff);
+  </script>
+  
+  <style scoped>
+  </style>
