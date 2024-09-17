262f12c544d88256303503c80fafb0e34613d836
Vik <victorgou.design@gmail.com>
2024-09-17 13:15:49 -0300

    Toggle and ToggleTheme created

---
+++ src/components/Toggle.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="toggle" :class="{ 'toggle-on': modelValue }" @click="toggle">
+      <div class="toggle-circle">
+        <component class="icon" :is="icon" />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    modelValue: {
+      type: Boolean,
+      required: true
+    },
+    icon: {
+      type: Object,
+      required: true
+    }
+  });
+  
+  const emit = defineEmits(['update:modelValue']);
+  
+  const toggle = () => {
+    emit('update:modelValue', !props.modelValue);
+  };
+  </script>
+  
+  <style scoped>
+  .toggle {
+    width: 60px;
+    height: 30px;
+    background-color: var(--c3);
+    border-radius: 15px;
+    position: relative;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  
+  .toggle-on {
+    background-color: var(--c3);
+  }
+  
+  .toggle-circle {
+    width: 26px;
+    height: 26px;
+    background-color: var(--c2);
+    border-radius: 50%;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: transform 0.3s;
+  }
+  
+  .toggle-on .toggle-circle {
+    transform: translateX(30px);
+  }
+  
+  .toggle-circle :deep(svg) {
+    width: 16px;
+    height: 16px;
+  }
+  .icon{
+    color: var(--c4);
+  }
+</style>
---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from 'vue';
+import Toggle from '@/components/Toggle.vue';
+import { Moon, Sun } from 'lucide-vue-next';
+import { currentTheme, toggleTheme } from '@/feature_modules/StyleVariables/theme';
+
+// Compute darkMode based on currentTheme
+const darkMode = computed({
+  get: () => currentTheme.value === 'dark',
+  set: (value) => toggleTheme()
+});
+
+// Compute the icon based on the current theme
+const toggleIcon = computed(() => darkMode.value ? Moon : Sun );
+</script>
+
+<template>
+  <Toggle v-model="darkMode" :icon="toggleIcon" />
+</template>
