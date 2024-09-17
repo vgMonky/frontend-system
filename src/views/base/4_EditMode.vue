be9849c184db4e9d6e2495250b8ac6986597ca45
Vik <victorgou.design@gmail.com>
2024-09-17 16:13:46 -0300

    EditMode is a Toggel now

--- src/views/base/4_EditMode.vue
+++ src/views/base/4_EditMode.vue
@@ -1,25 +1,31 @@
 <template>
-    <h1>Edit Mode</h1>
-    <button class="contained bold" @click="toggleEdit">
-      {{ isEditMode ? 'Turn Off Edit Mode' : 'Turn On Edit Mode' }}
-    </button>
+  <h1>Edit Mode</h1>
+  <p>asdds</p>
+  <div class="edit-mode-toggle contained">
+    <Toggle v-model="editMode" :icon="toggleIcon" />
     <br>
-    <p>Page Title:</p>
-    <p>Contact Link:</p>
-    <p>Contact Phone:</p>
-  </template>
-  
-  <script setup>
-  import { toggleEditMode, getEditMode } from '@/feature_modules/Navigation/toggleEditMode';
-  import { computed } from 'vue';
-  
-  const isEditMode = computed(() => getEditMode().value);
-  
-  function toggleEdit() {
-    toggleEditMode();
-  }
-  </script>
-  
-  <style scoped>
-  /* Your styles here */
-  </style>
+    <span>{{ editMode ? 'Edit Mode: On' : 'Edit Mode: Off' }}</span>
+  </div>
+  <br>
+  <p>Page Title:</p>
+  <p>Contact Link:</p>
+  <p>Contact Phone:</p>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import Toggle from '@/components/Toggle.vue';
+import {  MonitorCheck, Pencil} from 'lucide-vue-next';
+import { toggleEditMode, getEditMode } from '@/feature_modules/Navigation/toggleEditMode';
+
+const editMode = computed({
+  get: () => getEditMode().value,
+  set: () => toggleEditMode()
+});
+
+const toggleIcon = computed(() => editMode.value ? Pencil : MonitorCheck);
+</script>
+
+<style scoped>
+
+</style>
