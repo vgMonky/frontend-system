99db31fe02e62e59529a0a317c6f25e22d58c456
Vik <victorgou.design@gmail.com>
2024-09-20 20:43:16 -0300

    simple corrections

--- src/views/base/0_Intro.vue
+++ src/views/base/0_Intro.vue
@@ -3,7 +3,7 @@
     <em>
     <p class="ps">*** The filosofy of the constraints:  If you want to cut a tree, you use a Chainsaw. If you want to cut a paper you use Scissors. We dont design a single tool that cuts Trees and Papers. We create stuff convinient for the situation; we can say its "Designed by its Constraints". Of course the Scissors can be used in some creative way to do something else, but its design is shaped by its purpose.</p>
     <br>
-    <p class="ps">The system is designed in a strict manner. It is not intended to be infinitly costumizable, insted desicions where made to make this live template well suted for the constrains that define it: "Produce and present content in a fast manner". It lets the user create pages automatically by prompting over the system and controling global variables.  </p>
+    <p class="ps">The system is designed in a strict manner. It is not intended to be infinitly costumizable, insted desicions where made to make this live template well suted for the constrains that define it: "Produce and present content in a fast manner, with a global style perfectly aligned". It lets the user create pages automatically by prompting over the system and controling global variables. It also has a secundary mission, which is to serve as a base template for developers to extend functionalities. </p>
     <br>
     <br>
     <p class="ps">*** You can test the system live right now by following the workflow: <br>1.Style (The global variable style system) <br> 2.Components (The documented library) <br> 3. EditMode (Toggle a development environment) <br> 4.Create (Manual or with AI) </p>
