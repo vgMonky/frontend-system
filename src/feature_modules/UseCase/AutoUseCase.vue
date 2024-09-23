f3ad3cb4f7d5354e27fed4681666155292ac7030
Vik <victorgou.design@gmail.com>
2024-09-23 13:03:54 -0300

    src raw import removed

--- src/feature_modules/UseCase/AutoUseCase.vue
+++ src/feature_modules/UseCase/AutoUseCase.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="auto-use-case">
     <Expandable v-for="component in components" :key="component.name" :title="formatTitle(component.name)">
-      <SrcMode :sourceCode="component.source">
+      <SrcMode :sourceCode="'src code will go here'">
         <component :is="component.component" />
       </SrcMode>
     </Expandable>
@@ -9,40 +9,24 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import Expandable from '@/components/Expandable.vue';
 import SrcMode from '@/components/SrcMode.vue';
 
-// Import all components statically
-import UCbutton from './UCbutton.vue';
-import UCcontained from './UCcontained.vue';
-import UCExpandable from './UCExpandable.vue';
-import UCHero from './UCHero.vue';
-import UCHtmlInput from './UCHtmlInput.vue';
-import UCSrcMode from './UCSrcMode.vue';
-import UCtext from './UCtext.vue';
-import UCToggle from './UCToggle.vue';
+const components = ref([]);
 
-// Import all source code statically
-import UCbuttonSource from './UCbutton.vue?raw';
-import UCcontainedSource from './UCcontained.vue?raw';
-import UCExpandableSource from './UCExpandable.vue?raw';
-import UCHeroSource from './UCHero.vue?raw';
-import UCHtmlInputSource from './UCHtmlInput.vue?raw';
-import UCSrcModeSource from './UCSrcMode.vue?raw';
-import UCtextSource from './UCtext.vue?raw';
-import UCToggleSource from './UCToggle.vue?raw';
-
-const components = ref([
-  { name: 'UCbutton', component: UCbutton, source: UCbuttonSource },
-  { name: 'UCcontained', component: UCcontained, source: UCcontainedSource },
-  { name: 'UCExpandable', component: UCExpandable, source: UCExpandableSource },
-  { name: 'UCHero', component: UCHero, source: UCHeroSource },
-  { name: 'UCHtmlInput', component: UCHtmlInput, source: UCHtmlInputSource },
-  { name: 'UCSrcMode', component: UCSrcMode, source: UCSrcModeSource },
-  { name: 'UCtext', component: UCtext, source: UCtextSource },
-  { name: 'UCToggle', component: UCToggle, source: UCToggleSource },
-]);
+onMounted(async () => {
+  const componentModules = import.meta.glob('./*.vue', { eager: true });
+  
+  for (const path in componentModules) {
+    const name = path.split('/').pop().replace('.vue', '');
+    components.value.push({
+      name,
+      component: componentModules[path].default,
+      source: 'src code will go here'
+    });
+  }
+});
 
 const formatTitle = (name) => {
   // Remove the first two characters (UC) and then format
