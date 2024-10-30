e08e9947a898ed97af329002e8f7dad0cc87755e
vgMonky <victorgou.design@gmail.com>
2024-10-30 15:21:55 -0300

    Updates

---
+++ assets/0_EditMode-D62Xmv8x.js
@@ -0,0 +1,11 @@
+import{c as n,a as o,b as u,d as t,e as d,T as a,t as p,F as r,o as g,g as m,f as y}from"./index-CtgVCBqQ.js";/**
+ * @license lucide-vue-next v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=n("MonitorCheckIcon",[["path",{d:"m9 10 2 2 4-4",key:"1gnqz4"}],["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["path",{d:"M12 17v4",key:"1riwvh"}],["path",{d:"M8 21h8",key:"1ev6f3"}]]);/**
+ * @license lucide-vue-next v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=n("PencilIcon",[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]]),b={class:"contained edit-mode-toggle"},w={__name:"0_EditMode",setup(v){const e=o({get:()=>m().value,set:()=>y()}),i=o(()=>e.value?k:M);return(f,l)=>(g(),u(r,null,[l[4]||(l[4]=t("h1",null,"Edit Mode",-1)),l[5]||(l[5]=t("p",null,"If you toggle the edit mode, you will make the system hide all the editing capabilities. In this section you will also be able to set public variables....",-1)),l[6]||(l[6]=t("h3",null,"Toggle edit mode",-1)),l[7]||(l[7]=t("p",{class:"ps"},"Only if you set the correct password you can toggle the Edit Mode",-1)),l[8]||(l[8]=t("p",null,"Password:",-1)),l[9]||(l[9]=t("input",{type:"password"},null,-1)),t("div",b,[l[1]||(l[1]=t("p",null,"Alt + T to tuggle the EditMode. Then check the menu to notice the access change.",-1)),l[2]||(l[2]=t("br",null,null,-1)),d(a,{modelValue:e.value,"onUpdate:modelValue":l[0]||(l[0]=s=>e.value=s),icon:i.value},null,8,["modelValue","icon"]),l[3]||(l[3]=t("br",null,null,-1)),t("span",null,p(e.value?"Edit Mode: On":"Edit Mode: Off"),1)]),l[10]||(l[10]=t("br",null,null,-1)),l[11]||(l[11]=t("h3",null,"Public Variables",-1)),l[12]||(l[12]=t("p",null,"Page Title:",-1)),l[13]||(l[13]=t("input",{type:"text"},null,-1)),l[14]||(l[14]=t("p",null,"Page icon:",-1)),l[15]||(l[15]=t("input",{type:"file"},null,-1))],64))}};export{w as default};
---
+++ assets/Expandable-C7acNLf0.js
@@ -0,0 +1,11 @@
+import{c as l,_ as r,r as h,a as p,o as n,b as o,d as a,k as u,t as c,I as v,e as _,A as f,G as m,p as x,J as k}from"./index-CtgVCBqQ.js";/**
+ * @license lucide-vue-next v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=l("ChevronRightIcon",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-vue-next v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=l("RefreshCcwIcon",[["path",{d:"M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"14sxne"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}],["path",{d:"M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16",key:"1hlbsb"}],["path",{d:"M16 16h5v5",key:"ccwih5"}]]),g={class:"expandable"},y={class:"title"},C={class:"toggle-icon"},b={key:0,class:"content-container"},E={class:"content"},w={__name:"Expandable",props:{title:{type:String,default:"Title here"},titleopen:{type:String,default:""}},setup(t){const s=t,e=h(!1),i=()=>{e.value=!e.value};return p(()=>e.value&&s.titleopen?s.titleopen:s.title),(d,I)=>(n(),o("div",g,[a("div",{class:v(["title-container",{closed:!e.value}]),onClick:i},[a("p",y,[u(c(e.value&&t.titleopen||t.title)+" ",1),a("span",C,c(e.value?"▲":"▼"),1)])],2),_(k,{name:"expand"},{default:f(()=>[e.value?(n(),o("div",b,[a("div",E,[m(d.$slots,"default",{},void 0,!0)])])):x("",!0)]),_:3})]))}},S=r(w,[["__scopeId","data-v-9a732aa9"]]);export{M as C,S as E,N as R};
