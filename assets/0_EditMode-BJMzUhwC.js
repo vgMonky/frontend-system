ac27afa403cbe07f4f437aeb7ab83c56a21ea188
vgMonky <victorgou.design@gmail.com>
2024-11-07 16:46:05 -0300

    Updates

---
+++ assets/0_EditMode-BJMzUhwC.js
@@ -0,0 +1,11 @@
+import{c as n,a as o,b as d,d as e,e as s,T as a,t as p,F as r,o as g,g as m,f as y}from"./index-Busv214h.js";/**
+ * @license lucide-vue-next v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=n("MonitorCheckIcon",[["path",{d:"m9 10 2 2 4-4",key:"1gnqz4"}],["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["path",{d:"M12 17v4",key:"1riwvh"}],["path",{d:"M8 21h8",key:"1ev6f3"}]]);/**
+ * @license lucide-vue-next v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=n("PencilIcon",[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}],["path",{d:"m15 5 4 4",key:"1mk7zo"}]]),b={class:"contained edit-mode-toggle"},w={__name:"0_EditMode",setup(v){const t=o({get:()=>m().value,set:()=>y()}),i=o(()=>t.value?M:k);return(f,l)=>(g(),d(r,null,[l[4]||(l[4]=e("h1",null,"Edit Mode",-1)),l[5]||(l[5]=e("p",null,"If you toggle the edit mode, you will make the system hide all the editing capabilities. In this section you will also be able to set public variables....",-1)),l[6]||(l[6]=e("h3",null,"Toggle edit mode",-1)),l[7]||(l[7]=e("p",null,"Password:",-1)),l[8]||(l[8]=e("input",{type:"password"},null,-1)),e("div",b,[l[1]||(l[1]=e("p",null,"Toggle the EditMode. Then check the menu to notice the access change.",-1)),l[2]||(l[2]=e("br",null,null,-1)),s(a,{modelValue:t.value,"onUpdate:modelValue":l[0]||(l[0]=u=>t.value=u),icon:i.value},null,8,["modelValue","icon"]),l[3]||(l[3]=e("br",null,null,-1)),e("span",null,p(t.value?"Edit Mode: On":"Edit Mode: Off"),1)]),l[9]||(l[9]=e("br",null,null,-1)),l[10]||(l[10]=e("h3",null,"Public Variables",-1)),l[11]||(l[11]=e("p",null,"Page Title:",-1)),l[12]||(l[12]=e("input",{type:"text"},null,-1)),l[13]||(l[13]=e("p",null,"Page icon:",-1)),l[14]||(l[14]=e("input",{type:"file"},null,-1))],64))}};export{w as default};
